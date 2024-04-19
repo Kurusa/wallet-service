09ae385dcc9b7ad13edfbabb98163ffa3b246fae
Kurusa <kurusa03@gmail.com>
2024-04-19 18:29:11 +0300

    Update WalletBalanceDTO.php

--- app/DTO/WalletBalanceDTO.php
+++ app/DTO/WalletBalanceDTO.php
@@ -4,15 +4,9 @@ declare(strict_types=1);
 
 namespace App\DTO;
 
-class WalletBalanceDTO
+readonly class WalletBalanceDTO
 {
-    public string $currencyCode;
-
-    public int $balance;
-
     public function __construct(public string $currencyCode, public int $balance)
     {
-        $this->currencyCode = $currencyCode;
-        $this->balance = $balance;
     }
 }
